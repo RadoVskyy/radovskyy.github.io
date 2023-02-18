99f8cb672285ad074372440014432b12a9e1e11a
RadoVskyy <radek_molczan123@o2.pl>
2023-02-18 19:23:39 +0100

    w

--- bin/scripts/global.js
+++ bin/scripts/global.js
@@ -1,3 +1,10 @@
+// Variables from localstorage
+
+let music = localStorage.getItem("music");
+if (music == null){
+   music = "true";
+}
+
 // Loading screen
 
 var loading_screen = document.getElementById("loading-screen");
--- bin/start/index.js
+++ bin/start/index.js
@@ -17,3 +17,30 @@ window.onresize = function (event) {
        setTimeout(fullscreen, 500);
    }
 }
+
+// Options
+
+document.getElementById("options-button").onclick = function(){
+    if (document.getElementById("options").style.transform == "translate(0vw, -50%)"){
+        document.getElementById("options").style.transform = "translate(-35vw, -50%)"
+    }
+    else {
+        document.getElementById("options").style.transform = "translate(0vw, -50%)"
+    }
+}
+
+document.getElementById("music-button").onclick = function(){
+    if (music == "true"){
+        music = "false";
+        localStorage.setItem("music", music);
+        document.getElementById("music-button").style.color = "gray";
+        document.getElementById("music-button").style.border = "3px solid gray";
+    }
+    else if (music == "false"){
+        music = "true";
+        localStorage.setItem("music", music);
+        document.getElementById("music-button").style.color = "rgb(3, 201, 3)";
+        document.getElementById("music-button").style.border = "3px solid rgb(3, 201, 3)";
+    }
+}
+
